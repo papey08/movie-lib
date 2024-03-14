6498ecc8a55a4d62f9d3d056e958ee31d6959a4b
papey08 <popov.m4tvei@yandex.ru>
2024-03-14 17:59:41 +0300

    add creation of postgres database

---
+++ migrations/movie-lib-db-init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE "movies" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR(150),
+    "description" VARCHAR(1000),
+    "release_date" DATE,
+    "rating" FLOAT
+);
+
+CREATE TABLE "actors" (
+    "id" SERIAL PRIMARY KEY,
+    "first_name" VARCHAR(100),
+    "second_name" VARCHAR(100),
+    "gender" VARCHAR(10)
+);
+
+CREATE TABLE "movie-actor" (
+    "movie-id" INTEGER,
+    "actor_id" INTEGER,
+    UNIQUE ("movie-id", "actor_id")
+);
+
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "role" VARCHAR(10)
+);
+
+INSERT INTO "users" ("role")
+VALUES
+    ('admin'),
+    ('regular')
+;
