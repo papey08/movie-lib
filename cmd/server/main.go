2a7749e08f931e947191decc46453cde085292c6
papey08 <popov.m4tvei@yandex.ru>
2024-03-15 12:04:58 +0300

    add entrypoint & configs

---
+++ cmd/server/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/spf13/viper"
+	"movie-lib/internal/app"
+	"movie-lib/internal/ports/httpserver"
+	"movie-lib/internal/repo"
+	"movie-lib/pkg/logger"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func SetConfigs(configPath string) error {
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("cannot read config file %w", err)
+	}
+	return nil
+}
+
+func ConnectToPostgres(ctx context.Context) (*pgx.Conn, error) {
+	adsRepoUrl := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		viper.GetString("postgres-movie-lib.username"),
+		viper.GetString("postgres-movie-lib.password"),
+		viper.GetString("postgres-movie-lib.host"),
+		viper.GetInt("postgres-movie-lib.port"),
+		viper.GetString("postgres-movie-lib.dbname"),
+		viper.GetString("postgres-movie-lib.sslmode"))
+
+	// 30 attempts to connect to postgres when starting in docker container
+	for i := 0; i < 30; i++ {
+		conn, err := pgx.Connect(ctx, adsRepoUrl)
+		if err != nil {
+			time.Sleep(time.Second)
+		} else {
+			return conn, nil
+		}
+	}
+
+	return nil, errors.New("unable to connect to postgres ads repo")
+}
+
+const (
+	dockerConfigFile = "config/config-docker.yml"
+	localConfigFile  = "config/config-local.yml"
+)
+
+func main() {
+	ctx := context.Background()
+	logs := logger.DefaultLogger(os.Stdout)
+
+	isDocker := flag.Bool("docker", false, "flag if this project is running in docker container")
+	flag.Parse()
+	var configPath string
+	if *isDocker {
+		configPath = dockerConfigFile
+	} else {
+		configPath = localConfigFile
+	}
+
+	if err := SetConfigs(configPath); err != nil {
+		logs.FatalLog(fmt.Sprintf("reading configs: %s", err.Error()))
+	}
+
+	conn, err := ConnectToPostgres(ctx)
+	if err != nil {
+		logs.FatalLog(fmt.Sprintf("connecting to postgres: %s", err.Error()))
+	}
+	logs.InfoLog("successfully connected to postgres")
+
+	srvAddr := fmt.Sprintf("%s:%d",
+		viper.GetString("http-server.host"),
+		viper.GetInt("http-server.port"),
+	)
+
+	r := repo.New(conn)
+	a := app.New(r, logs)
+
+	srv := httpserver.New(ctx, srvAddr, a, logs)
+
+	go func() {
+		_ = srv.ListenAndServe()
+	}()
+	logs.InfoLog("http server successfully started")
+
+	// preparing graceful shutdown
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGINT)
+
+	// waiting for Ctrl+C
+	<-osSignals
+
+	shutdownCtx, cancel := context.WithTimeout(ctx, 30*time.Second) // 30s timeout to finish all active connections
+	defer cancel()
+
+	_ = srv.Shutdown(shutdownCtx)
+	logs.InfoLog("successfully stopped http server")
+}
